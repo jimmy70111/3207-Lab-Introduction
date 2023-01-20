c19fa60b092b10223e68617fbe2bf6a0f65a48a3
JimmyJiang <tul70511@temple.edu>
2023-01-19 21:32:34 -0500

    testing

--- print_random.c
+++ print_random.c
@@ -6,17 +6,6 @@
 
 int main()
 {
-	int a;
-	
-
-	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
-
-	printf("Today's random word: ");
-	for(a=0;a<7;a++)
-	putchar( randchar() );
-	putchar('\n');
-
-	return(0);
+printf("hello");
 }
 
