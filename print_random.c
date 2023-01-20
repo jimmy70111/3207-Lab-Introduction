53cdf8e9ebb35665671e4562a29194626842957d
JimmyJiang <tul70511@temple.edu>
2023-01-19 21:36:37 -0500

    was testing vs code

--- print_random.c
+++ print_random.c
@@ -6,6 +6,17 @@
 
 int main()
 {
-printf("hello");
+	int a;
+	
+
+	/* seed the randomizer */
+	srand( (unsigned)time(NULL) );
+
+	printf("Today's random word: ");
+	for(a=0;a<7;a++)
+	putchar( randchar() );
+	putchar('\n');
+
+	return(0);
 }
 
